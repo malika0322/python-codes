051979b2f48906a79474a086cab6997c9217860b
malika0322 <budhathokimalika@gmail.com>
2024-09-11 10:33:47 +0545

    removed assignment folder from repository

--- assignment/2.py
+++
@@ -1,5 +0,0 @@
-'''2. WAP to enter a number from user and print its absolute value 
--1 => 1 
--3 => 3'''
-num = int(input("Enter a number: "))
-print(f"The absolute value is {abs(num)}.")
--- assignment/5.py
+++
@@ -1,2 +0,0 @@
-'''5. WAP to enter a number from user and print sum of its individual digits 
-153 --> 9'''
